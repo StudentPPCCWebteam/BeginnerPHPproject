044e4dd42b57e63d376ed40dc8efaa955499216f
StudentPPCCWebteam <42656513+StudentPPCCWebteam@users.noreply.github.com>
2018-08-23 15:09:21 -0600

    Create Login_delete.php

---
+++ Login_delete.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+
+
+
+<?php include 'db.php'; ?>
+<?php include 'functions.php'; ?>
+<?php
+if (isset($_POST['submit'])) {
+    deleteRows();
+}
+?>
+
+
+
+
+
+<div class="container">
+    <div class="col-xs-6">
+        <form action="login_delete.php" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <div class="form-group">
+                <select name="id" id="">
+                    <?php
+                    showAllData();
+                    ?>
+                </select>
+            </div>
+            <input class="btn btn-primary" type="submit" name="submit" value="DELETE">
+        </form>
+    </div>
+</div>
+</body>
+</html>
