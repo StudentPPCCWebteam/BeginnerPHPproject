d8d0a322ba727436ed2a61db5490fc80442979f6
StudentPPCCWebteam <42656513+StudentPPCCWebteam@users.noreply.github.com>
2018-08-23 15:07:54 -0600

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,63 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+
+
+
+<?php
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password =  $_POST['password'];
+        $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+        if($connection) {
+            echo 'we connected';
+        } else {
+            die('Database connection fail');
+        }
+//            if($username && $password) {
+//                echo $username . '<br>';
+//                echo $password;
+//            } else {
+//                echo 'no';
+//            }
+
+
+    }
+
+
+
+?>
+
+
+
+
+
+
+
+
+    <div class="container">
+        <div class="col-xs-6">
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
